28460bc0b541ab6b57193fa3b044fefede2d8d41
xeops <xeops123@mail.ru>
2021-06-01 18:10:04 +0300

    Create DrinkableCollection.php

---
+++ DrinkableCollection.php
@@ -0,0 +1,38 @@
+<?php
+
+Interface IDrinkable{}
+
+class Cup implements IDrinkable
+{
+	public function __construct()
+	{
+	}
+}
+
+class Bottle  implements IDrinkable
+{
+	public function __construct()
+	{
+	}
+}
+
+class Glass  implements IDrinkable
+{
+	public function __construct()
+	{
+	}
+}
+
+function display()
+{
+	$list = [
+		new Cup(),
+		new Bottle()
+	];
+
+	foreach ($list as $item)
+	{
+		echo get_class($item) . PHP_EOL;
+	}
+}
+display();
